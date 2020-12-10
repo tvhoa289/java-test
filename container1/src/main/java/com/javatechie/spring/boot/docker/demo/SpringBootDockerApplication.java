f1518d22852691a42646f13f6a7d27f9d6f5d9fd
tvhoa <tvhoa@sutrix>
2020-12-10 14:03:38 +0700

    add container1

---
+++ container1/src/main/java/com/javatechie/spring/boot/docker/demo/SpringBootDockerApplication.java
@@ -0,0 +1,21 @@
+package com.javatechie.spring.boot.docker.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootDockerApplication {
+
+	@GetMapping("/notification")
+	public String getMessage() {
+		return "This is Notification PAGE";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerApplication.class, args);
+	}
+
+}
