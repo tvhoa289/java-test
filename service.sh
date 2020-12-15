720499527384f408218a58d7b4f1f6b63e9724dd
Truong Van Hoa <hoa.truong@sutrixsolutions.com>
2020-12-15 10:31:24 +0700

    ccc

--- service.sh
+++ service.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 echo "${Choice_ServiceName}"
+aws cloudformation create-stack --stack-name ${Choice_ServiceName} --template-body file://service1.yml 
+--parameters ServiceName=${Choice_ServiceName} ContainerCpu=${Choice_CPU} ContainerMemory=${Choice_Memory}
+ContainerPort=${Port_Reference} Family=${Family_reference} TargetGroupName=${Targetgroup_Name} Path=${Path_healcheck}
+ImageUrl=${Image_Url_reference}
