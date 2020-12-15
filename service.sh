a2c874c1b30e000edbe00849eef4e93bc583de61
Truong Van Hoa <hoa.truong@sutrixsolutions.com>
2020-12-15 13:58:31 +0700

    asda

--- service.sh
+++ service.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 echo "${Choice_ServiceName}"
+sed s/LoadBalancerRuleApp/LoadBalancerRule${Choice_ServiceName}/g service.yaml
+sed s/TargetGroupApp/TargetGroup${Choice_ServiceName}/g service.yaml
+sed s/TaskDefinitionApp/TaskDefinition${Choice_ServiceName}/g service.yaml
+sed s/ServiceApp/Service${Choice_ServiceName}/g service.yaml
+
 
 aws cloudformation create-stack --stack-name ${Choice_ServiceName} --template-body file://service1.yaml --parameters ParameterKey=ServiceName,ParameterValue=${Choice_ServiceName} ParameterKey=ContainerName,ParameterValue=${Choice_ServiceName} ParameterKey=ContainerCpu,ParameterValue=${Choice_CPU} ParameterKey=ContainerMemory,ParameterValue=${Choice_Memory} ParameterKey=ContainerPort,ParameterValue=${Port_Reference} ParameterKey=Family,ParameterValue=${Family_reference} ParameterKey=TargetGroupName,ParameterValue=${Targetgroup_Name} ParameterKey=Path,ParameterValue=${Path_healcheck} ParameterKey=ImageUrl,ParameterValue=${Image_Url_reference}
