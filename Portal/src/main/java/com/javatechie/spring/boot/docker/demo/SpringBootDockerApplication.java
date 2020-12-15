8e68312fc206d4526c642a09af18d9f962e2259c
tvhoa289 <73008989+tvhoa289@users.noreply.github.com>
2020-12-15 16:50:22 +0700

    Update SpringBootDockerApplication.java

--- Portal/src/main/java/com/javatechie/spring/boot/docker/demo/SpringBootDockerApplication.java
+++ Portal/src/main/java/com/javatechie/spring/boot/docker/demo/SpringBootDockerApplication.java
@@ -11,7 +11,7 @@ public class SpringBootDockerApplication {
 
 	@GetMapping("/portal")
 	public String getMessage() {
-		return "This is portal PAGE";
+		return "This is portal  update1 PAGE";
 	}
 
 	public static void main(String[] args) {
