2985cfb9a531a7d4588b5b10466e1a480f39cf6d
Truong Van Hoa <hoa.truong@sutrixsolutions.com>
2020-12-15 11:16:07 +0700

    asdah

--- service.sh
+++ service.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "${Choice_ServiceName}"
-echo ${(t) ${Port_Reference}}
+echo "${(t) ${Port_Reference}}"
 aws cloudformation create-stack --stack-name ${Choice_ServiceName} --template-body file://service1.yaml --parameters ParameterKey=ServiceName,ParameterValue=${Choice_ServiceName} ParameterKey=ContainerName,ParameterValue=${Choice_ServiceName} ParameterKey=ContainerCpu,ParameterValue=${Choice_CPU} ParameterKey=ContainerMemory,ParameterValue=${Choice_Memory} ParameterKey=ContainerPort,ParameterValue=${Port_Reference} ParameterKey=Family,ParameterValue=${Family_reference} ParameterKey=TargetGroupName,ParameterValue=${Targetgroup_Name} ParameterKey=Path,ParameterValue=${Path_healcheck} ParameterKey=ImageUrl,ParameterValue=${Image_Url_reference}
