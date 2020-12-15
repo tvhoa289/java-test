07ad2b6dfced0a2fe3f6759701e04230062a134b
Truong Van Hoa <hoa.truong@sutrixsolutions.com>
2020-12-15 10:40:58 +0700

    sasda

--- service.sh
+++ service.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 echo "${Choice_ServiceName}"
-aws cloudformation create-stack --stack-name ${Choice_ServiceName} --template-body file://service1.yml 
---parameters ServiceName=${Choice_ServiceName} ContainerCpu=${Choice_CPU} ContainerMemory=${Choice_Memory}
-ContainerPort=${Port_Reference} Family=${Family_reference} TargetGroupName=${Targetgroup_Name} Path=${Path_healcheck}
-ImageUrl=${Image_Url_reference}
+aws cloudformation create-stack --stack-name ${Choice_ServiceName} --template-body file://service1.yml --parameters ServiceName=${Choice_ServiceName} ContainerCpu=${Choice_CPU} ContainerMemory=${Choice_Memory} ContainerPort="${Port_Reference}" Family=${Family_reference} TargetGroupName=${Targetgroup_Name} Path=${Path_healcheck} ImageUrl=${Image_Url_reference}
