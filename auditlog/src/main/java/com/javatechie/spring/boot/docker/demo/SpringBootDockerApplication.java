50cc382d1ccee4984c0d82c8fe92c400663fd36e
tvhoa289 <73008989+tvhoa289@users.noreply.github.com>
2020-12-18 15:52:09 +0700

    Update SpringBootDockerApplication.java

--- auditlog/src/main/java/com/javatechie/spring/boot/docker/demo/SpringBootDockerApplication.java
+++ auditlog/src/main/java/com/javatechie/spring/boot/docker/demo/SpringBootDockerApplication.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SpringBootDockerApplication {
 
-	@GetMapping("/Authentication")
+	@GetMapping("/auditlog")
 	public String getMessage() {
-		return "This is Authentication update1000000 PAGE";
+		return "This is AuditLOG PAGE";
 	}
 
 	public static void main(String[] args) {
