2ecfa791a79c7732088471c928fde6b7b8fb0552
Truong Van Hoa <hoa.truong@sutrixsolutions.com>
2020-12-18 15:51:07 +0700

    service update

---
+++ admin/src/main/java/com/javatechie/spring/boot/docker/demo/SpringBootDockerApplication.java
@@ -0,0 +1,21 @@
+package com.javatechie.spring.boot.docker.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootDockerApplication {
+
+	@GetMapping("/Authentication")
+	public String getMessage() {
+		return "This is Authentication update1000000 PAGE";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerApplication.class, args);
+	}
+
+}
