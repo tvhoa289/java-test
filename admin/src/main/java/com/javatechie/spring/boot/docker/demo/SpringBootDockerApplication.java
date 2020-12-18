e0b74f4aa1eaa59f31ea64ad8dcea2183c9a4dc6
tvhoa289 <73008989+tvhoa289@users.noreply.github.com>
2020-12-18 15:52:55 +0700

    Update SpringBootDockerApplication.java

--- admin/src/main/java/com/javatechie/spring/boot/docker/demo/SpringBootDockerApplication.java
+++ admin/src/main/java/com/javatechie/spring/boot/docker/demo/SpringBootDockerApplication.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SpringBootDockerApplication {
 
-	@GetMapping("/Authentication")
+	@GetMapping("/admin")
 	public String getMessage() {
-		return "This is Authentication update1000000 PAGE";
+		return "This is Adminstrator PAGE";
 	}
 
 	public static void main(String[] args) {
