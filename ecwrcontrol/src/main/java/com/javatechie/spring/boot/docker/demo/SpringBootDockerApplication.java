9868cbd8414a6629a02852d5fa7f60ef352af2a8
tvhoa289 <73008989+tvhoa289@users.noreply.github.com>
2020-12-18 15:55:07 +0700

    Update SpringBootDockerApplication.java

--- ecwrcontrol/src/main/java/com/javatechie/spring/boot/docker/demo/SpringBootDockerApplication.java
+++ ecwrcontrol/src/main/java/com/javatechie/spring/boot/docker/demo/SpringBootDockerApplication.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SpringBootDockerApplication {
 
-	@GetMapping("/Authentication")
+	@GetMapping("/ecwrcontrol")
 	public String getMessage() {
-		return "This is Authentication update1000000 PAGE";
+		return "This is ecwrcontrol  PAGE";
 	}
 
 	public static void main(String[] args) {
