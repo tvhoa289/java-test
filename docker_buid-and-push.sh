fbd0501ae1c7c7d9bcff0fd18d456d8bc139dbee
Truong Van Hoa <hoa.truong@sutrixsolutions.com>
2020-12-23 11:54:57 +0700

    ads

--- docker_buid-and-push.sh
+++ docker_buid-and-push.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
 
-authentication = 'tvhoa289/spring-boot-authentication-p8081'
-portal='aaa'
-document_upload ="tvhoa289/spring-boot-documentupload-p8088"
-cache_management ="tvhoa289/spring-boot-cachemanagement-p8089"
-notification ="tvhoa289/spring-boot-notification-p8765"
-user = 'tvhoa289/spring-boot-user-p8080'
-auditlog ="tvhoa289/spring-boot-auditlog-p8083"
-dataquery ="tvhoa289/spring-boot-dataquery-p8084"
-ecwrcontrol = "tvhoa289/spring-boot-ecwrcontrol-p8085"
-configuration = "tvhoa289/spring-boot-configuration-p8086"
-admin = "tvhoa289/spring-boot-admin-p8087"
-dataImport = "tvhoa289/spring-boot-dataImport-p8888"
+authentication="tvhoa289/spring-boot-authentication-p8081"
+portal="tvhoa289/spring-boot-portal-p8082"
+document_upload="tvhoa289/spring-boot-documentupload-p8088"
+cache_management="tvhoa289/spring-boot-cachemanagement-p8089"
+notification="tvhoa289/spring-boot-notification-p8765"
+user="tvhoa289/spring-boot-user-p8080"
+auditlog="tvhoa289/spring-boot-auditlog-p8083"
+dataquery="tvhoa289/spring-boot-dataquery-p8084"
+ecwrcontrol="tvhoa289/spring-boot-ecwrcontrol-p8085"
+configuration="tvhoa289/spring-boot-configuration-p8086"
+admin="tvhoa289/spring-boot-admin-p8087"
+dataImport="tvhoa289/spring-boot-dataImport-p8888"
 #Docker build and push
 
 echo "$BUILD_NUMBER"
 echo "${Choice_Image_to_build}"
 echo $portal
-echo "$portal"
-echo "$user"
+echo $user
 
 if [ "${Choice_Image_to_build}" == "authentication" ]
     then
