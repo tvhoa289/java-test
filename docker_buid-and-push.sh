474446c8b5951dc13a633b230bd273512d393bc1
Truong Van Hoa <hoa.truong@sutrixsolutions.com>
2020-12-23 09:46:50 +0700

    ads

--- docker_buid-and-push.sh
+++ docker_buid-and-push.sh
@@ -24,12 +24,12 @@ if [ "${Choice_Image_to_build}" == "authentication" ]
         docker build -t $authentication:$BUILD_NUMBER --build-arg path=/authentication/target/spring-boot-docker.jar --build-arg port=8081 .
         docker push $authentication:$BUILD_NUMBER
         docker rmi $authentication:$BUILD_NUMBER
-elif "${Choice_Image_to_build}" == "document_upload"
+elif [\"${Choice_Image_to_build}\" == \"document_upload\"]
     then
         docker build -t $document_upload:$BUILD_NUMBER --build-arg path=/documentupload/target/spring-boot-docker.jar --build-arg port=8088 .
         docker push $document_upload:$BUILD_NUMBER
         docker rmi $document_upload:$BUILD_NUMBER
-elif [ "${Choice_Image_to_build}" == "portal" ];
+elif [ \"${Choice_Image_to_build}\" == \"portal\" ];
     then
         docker build -t $portal:$BUILD_NUMBER --build-arg path=/Portal/target/spring-boot-docker.jar --build-arg port=8082 .
         docker push $portal:$BUILD_NUMBER
