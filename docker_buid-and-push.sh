a111a7a17818ad9ebe8cc6c520a5d13bb67a4ee3
Truong Van Hoa <hoa.truong@sutrixsolutions.com>
2020-12-23 11:35:44 +0700

    ads

--- docker_buid-and-push.sh
+++ docker_buid-and-push.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 
-authentication = "tvhoa289/spring-boot-authentication-p8081"
-portal = "tvhoa289/spring-boot-portal-p8082"
-document_upload = "tvhoa289/spring-boot-documentupload-p8088"
-cache_management = "tvhoa289/spring-boot-cachemanagement-p8089"
-notification = "tvhoa289/spring-boot-notification-p8765"
-user = "tvhoa289/spring-boot-user-p8080"
-auditlog = "tvhoa289/spring-boot-auditlog-p8083"
-dataquery = "tvhoa289/spring-boot-dataquery-p8084"
+authentication ="tvhoa289/spring-boot-authentication-p8081"
+portal ="tvhoa289/spring-boot-portal-p8082"
+document_upload ="tvhoa289/spring-boot-documentupload-p8088"
+cache_management ="tvhoa289/spring-boot-cachemanagement-p8089"
+notification ="tvhoa289/spring-boot-notification-p8765"
+user ="tvhoa289/spring-boot-user-p8080"
+auditlog ="tvhoa289/spring-boot-auditlog-p8083"
+dataquery ="tvhoa289/spring-boot-dataquery-p8084"
 ecwrcontrol = "tvhoa289/spring-boot-ecwrcontrol-p8085"
 configuration = "tvhoa289/spring-boot-configuration-p8086"
 admin = "tvhoa289/spring-boot-admin-p8087"
@@ -18,6 +18,7 @@ dataImport = "tvhoa289/spring-boot-dataImport-p8888"
 echo "$BUILD_NUMBER"
 echo "${Choice_Image_to_build}"
 echo $portal
+echo $user
 
 if [ "${Choice_Image_to_build}" == "authentication" ]
     then
