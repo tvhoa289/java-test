d6c51d6ee4b52ade519f5691040f861269a01fcd
Truong Van Hoa <hoa.truong@sutrixsolutions.com>
2020-12-23 11:01:10 +0700

    ads

--- docker_buid-and-push.sh
+++ docker_buid-and-push.sh
@@ -31,7 +31,7 @@ elif [ "${Choice_Image_to_build}" == "documentupload" ]
         docker rmi $document_upload:$BUILD_NUMBER
 elif [ "${Choice_Image_to_build}" == "portal" ]
     then
-        docker build -t $portal:$BUILD_NUMBER --build-arg path=/portal/target/spring-boot-docker.jar --build-arg port=8082 .
+        docker build --build-arg path=/portal/target/spring-boot-docker.jar --build-arg port=8082 -t $portal:$BUILD_NUMBER -f Dockerfile
         docker push $portal:$BUILD_NUMBER
         docker rmi $portal:$BUILD_NUMBER
         
