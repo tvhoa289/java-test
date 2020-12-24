76d3735bd18f1ef5980c8c4a0c7fd936efdb7c6a
Truong Van Hoa <hoa.truong@sutrixsolutions.com>
2020-12-24 11:05:59 +0700

    ads

--- createRepo.sh
+++ createRepo.sh
@@ -5,5 +5,5 @@ aws ecr create-repository --repository-name lssp-documentupload --image-tag-muta
 aws ecr create-repository --repository-name lssp-portal --image-tag-mutability IMMUTABLE  --region ap-southeast-1 >>repo.txt
 aws ecr create-repository --repository-name lssp-auditlog --image-scanning-configuration --image-tag-mutability IMMUTABLE --region ap-southeast-1 >>repo.txt
 repo="$(grep -oP '(?<="repositoryUri": ")[^"]*' repo.txt)"
-cho "image URL for Repository"
-echo '$repo'
+echo "image URL for Repository"
+echo $repo
