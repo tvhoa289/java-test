e84b712c21e50fd5827d14441536fcc04e4b349e
Truong Van Hoa <hoa.truong@sutrixsolutions.com>
2020-12-24 11:18:47 +0700

    ads

--- createRepo.sh
+++ createRepo.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-aws ecr create-repository --repository-name lssp-documentupload --image-tag-mutability IMMUTABLE --image-scanning-configuration scanOnPush=true --region ap-southeast-1 > repo.txt
-aws ecr create-repository --repository-name lssp-portal --image-tag-mutability IMMUTABLE  --region ap-southeast-1 >>repo.txt
-aws ecr create-repository --repository-name lssp-auditlog --image-tag-mutability IMMUTABLE --region ap-southeast-1 >>repo.txt
+aws ecr create-repository --repository-name lssp-documentupload-p8088 --image-tag-mutability IMMUTABLE --image-scanning-configuration scanOnPush=true --region ap-southeast-1 > repo.txt
+aws ecr create-repository --repository-name lssp-portal-p8082 --image-tag-mutability IMMUTABLE  --region ap-southeast-1 >>repo.txt
+aws ecr create-repository --repository-name lssp-auditlog-p8083 --image-tag-mutability IMMUTABLE --region ap-southeast-1 >>repo.txt
 repo="$(grep -oP '(?<="repositoryUri": ")[^"]*' repo.txt)"
 echo "image URL for Repository"
 grep -oP '(?<="repositoryUri": ")[^"]*' repo.txt
